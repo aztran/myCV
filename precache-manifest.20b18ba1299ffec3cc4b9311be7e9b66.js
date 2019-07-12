2df5d384a459c7705b25945b71cb60f894fe734c
aztran <andra.riztyan@gmail.com>
2019-07-12 17:08:24 +0700

    Updates

---
+++ precache-manifest.20b18ba1299ffec3cc4b9311be7e9b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d946592792370a514a0edfd69205cf3",
+    "url": "/aztran.github.io/index.html"
+  },
+  {
+    "revision": "46eed0d178d93fee2d8d",
+    "url": "/aztran.github.io/static/css/2.bc5e0df1.chunk.css"
+  },
+  {
+    "revision": "ba59480ed533c1687421",
+    "url": "/aztran.github.io/static/css/main.7b03f9a5.chunk.css"
+  },
+  {
+    "revision": "46eed0d178d93fee2d8d",
+    "url": "/aztran.github.io/static/js/2.b6caae67.chunk.js"
+  },
+  {
+    "revision": "ba59480ed533c1687421",
+    "url": "/aztran.github.io/static/js/main.2ec784c7.chunk.js"
+  },
+  {
+    "revision": "f9081c6a315af315b936",
+    "url": "/aztran.github.io/static/js/runtime~main.cbbf4668.js"
+  },
+  {
+    "revision": "fca0c4633e3584b428f6fe66179cf47a",
+    "url": "/aztran.github.io/static/media/ava.fca0c463.png"
+  }
+]);
